fb7d6e6c672eec7b9bc25319c6d34e19af5c5eea
Mehmet T Kuyucu <you@example.com>
2023-01-21 20:26:24 +0100

    Initial Commit

---
+++ PrayTime/PrayTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayTimeApp.swift
+//  PrayTime
+//
+//  Created by Mehmet Tayyar Kuyucu on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PrayTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
